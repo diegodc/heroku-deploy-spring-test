48339493a4e8e3c31de16051c7e10d956dc49006
diegodc <diego.dc.84@hotmail.com>
2018-06-07 19:57:24 -0300

    Initial commit

---
+++ src/main/java/app/Controller.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("test")
+public class Controller {
+
+    @GetMapping
+    public String cipherMessage() {
+        return "Working!";
+    }
+
+}
