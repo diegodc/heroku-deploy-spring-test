8477d0f1e024d59bbc2326b830c57c0585c4adc2
diegodc <diego.dc.84@hotmail.com>
2018-06-07 20:08:49 -0300

    auto deploy test

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -2,13 +2,21 @@ package app;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Properties;
+
 @RestController
 @RequestMapping("test")
 public class Controller {
 
     @GetMapping
-    public String cipherMessage() {
+    public String get() {
         return "Working Fine!";
+    }
+
+    @GetMapping(value = "/properties")
+    public Properties getProperties() {
+
+        return System.getProperties();
     }
 
 }
