411c18c4f53cc06350a451e5ab07e20ce8ae641d
diegodc <diego.dc.84@hotmail.com>
2018-06-07 21:18:44 -0300

    file test

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,7 +1,10 @@
 package app;
 
+import org.hibernate.validator.internal.metadata.location.FieldConstraintLocation;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
@@ -22,6 +25,23 @@ public class Controller {
     @GetMapping(value = "/environment")
     public Map<String, String> getEnvironment() {
         return System.getenv();
+    }
+
+    @GetMapping(value = "/file")
+    public String fileTest() {
+        File file = new File("test");
+
+        if (file.exists()) {
+            file.delete();
+        }
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return String.valueOf(file.exists());
     }
 
 }
